decd52ba6d89752fe89e6542803e30bc663dd887
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-07-21 21:21:31 +0300

    Создание кода расчета бонуса

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        long replenishment_amount = 1350;
+        int present = 100;
+        long bonus = replenishment_amount * present / 100 / 100;
+        System.out.println(bonus);
+
+    }
+}
