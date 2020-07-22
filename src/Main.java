f0b978b508c11843e5fc7d5f00799862193e5dc0
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-07-22 20:27:50 +0300

    Добавление условного оператора в код расчета бонусов.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main (String[] args) {
-        long replenishment_amount = 1350;
+        long replenishmentAmount = 1500;
         int present = 100;
-        long bonus = replenishment_amount * present / 100 / 100;
+        long bonus = replenishmentAmount * present / 100 / 100;
+        if (replenishmentAmount >= 1000)
         System.out.println(bonus);
-
     }
 }
