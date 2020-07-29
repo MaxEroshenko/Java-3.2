291426a1369ebbf651d441564a2e8c6d1911b5a5
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-07-30 00:23:26 +0300

    Исправление переменной if и исправление формулы

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        long deceivingSum = 100;
+        long accountAmount = 100;
         long replenishmentAmount = 1200;
         long limit = 1000;
         long bonus = 0;
         if (replenishmentAmount >= limit)
             bonus = replenishmentAmount / 100;
-        long totalAmount = deceivingSum + replenishmentAmount + bonus;
+        long totalAmount = accountAmount + replenishmentAmount + bonus;
         System.out.println(" Бонус " + bonus);
         System.out.println(" Итоговая сумма " + totalAmount);
     }
